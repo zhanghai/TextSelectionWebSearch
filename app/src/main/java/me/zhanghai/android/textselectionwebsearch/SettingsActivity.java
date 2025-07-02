e1d4efc36d8502e1271419a218d3381eae569c35
Hai Zhang <dreaming.in.code.zh@gmail.com>
2025-07-02 16:20:25 -0700

    Fix: Fix issue due to edge-to-edge enforcement on Android V

--- app/src/main/java/me/zhanghai/android/textselectionwebsearch/SettingsActivity.java
+++ app/src/main/java/me/zhanghai/android/textselectionwebsearch/SettingsActivity.java
@@ -6,11 +6,13 @@
 package me.zhanghai.android.textselectionwebsearch;
 
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceScreen;
 import android.text.TextUtils;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -26,6 +28,14 @@ public class SettingsActivity extends PreferenceActivity
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (Build.VERSION.SDK_INT == Build.VERSION_CODES.VANILLA_ICE_CREAM) {
+            // Work around the issue that the fix for edge-to-edge enforcement
+            // https://android.googlesource.com/platform/frameworks/base/+/213033ecf41f17aebd766abfa7af7c789f245ff7
+            // is only available on V QPR2 and above.
+            final ViewGroup contentView = requireViewById(android.R.id.content);
+            contentView.getChildAt(0).setFitsSystemWindows(true);
+        }
 
         addPreferencesFromResource(R.xml.settings);
 
